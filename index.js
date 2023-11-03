df0469f66c6fd73737077578db322cfc3a1ec32a
Riborn5 <102695215+Riborn5@users.noreply.github.com>
2023-11-04 02:48:03 +0300

    Add three.js webgl and json fonts support

--- index.js
+++ index.js
@@ -1,11 +1,8 @@
 import { createRoot } from 'react-dom/client';
 
-function NavigationBar() {
-  // TODO: Actually implement a navigation bar
-  <a href="main_page.html">Home</a>
-  return <h1>Hello from React!</h1>;
-}
+// Clear the existing HTML content
+document.body.innerHTML = '<div id="app"></div>';
 
-const domNode = document.getElementById('navigation');
-const root = createRoot(domNode);
-root.render(<NavigationBar />);
+// Render your React component instead
+const root = createRoot(document.getElementById('app'));
+root.render(<h1>Hello, wodrld</h1>);
